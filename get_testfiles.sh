b216004316c3466e62cd430d163bd2b8b203d7b0
Rico Pajarola <rp@servium.ch>
2025-02-03 08:57:51 +0100

    fix get_testfiles.sh

--- get_testfiles.sh
+++ get_testfiles.sh
@@ -5,10 +5,8 @@ git annex init || echo "git annex not initialized"
 urlbase=https://cave.servium.ch/github/exiftools
 git annex list | while read where fname; do
   case "${where}:${fname}" in
-  *X:testdata/*)
-    echo "${fname}"
+  ??_??:*)
     git annex addurl --file "${fname}" "${urlbase}/${fname}"
-    ;;
   esac
 done
 
